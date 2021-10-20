eaade322dbd4910c4ea59e51b9dd1591f7178e8f
putin <putinczev52@gmail.com>
2021-10-20 19:57:59 +0300

    Добавьте файлы проекта.

---
+++ PR3TRPO_Put/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PR3TRPO_Put
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
